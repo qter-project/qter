13be6c86568fda89025dfd00930b216ec385aa05
Arhan Chaudhary <arhan.ch@gmail.com>
2025-12-28 12:23:27 -0800

    chore(robot_app): set up leptos

--- src/robot_app/src/main.rs
+++ src/robot_app/src/main.rs
@@ -1,3 +1,12 @@
+use leptos::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    leptos::mount::mount_to_body(|| {
+        view! {
+            <p>"Hello, world!"</p>
+        }
+    })
 }
